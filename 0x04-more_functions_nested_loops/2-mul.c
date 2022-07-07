1533a55e1c1f1aba93bcd41bebb426bad1d5d1fe
Okezie-Emmanuel <106774632+Okezie-Emmanuel@users.noreply.github.com>
2022-07-08 00:10:57 +0100

    Create 2-mul.c
    Colabo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers and returns the output
+ * _putchar - Gives the desired output
+ * @a: char type int
+ * @b: char type int
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
